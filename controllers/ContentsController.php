8d78386a9d97184b949f81f51fdfadde46cf8f1f
David Persson <davidpersson@gmx.de>
2015-06-10 14:20:20 +0200

    Allow deleting content.

--- controllers/ContentsController.php
+++ controllers/ContentsController.php
@@ -20,6 +20,7 @@ class ContentsController extends \base_core\controllers\BaseController {
 
 	use \base_core\controllers\AdminIndexTrait;
 	use \base_core\controllers\AdminEditTrait;
+	use \base_core\controllers\AdminDeleteTrait;
 	use \base_core\controllers\AdminPublishTrait;
 
 	public function admin_add() {
