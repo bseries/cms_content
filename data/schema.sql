70cfe72e61fcc21c484684193bd4140eb5494ac6
David Persson <davidpersson@gmx.de>
2016-02-29 13:01:25 +0100

    Up schema.

--- data/schema.sql
+++ data/schema.sql
@@ -1,8 +1,11 @@
-CREATE TABLE `contents` (
+CREATE TABLE `content_blocks` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `owner_id` int(11) unsigned NOT NULL,
   `type` varchar(20) NOT NULL DEFAULT '',
   `region` varchar(100) NOT NULL DEFAULT '',
   `value_text` text,
+  `value_number` int(20) DEFAULT NULL,
+  `value_money` int(20) DEFAULT NULL,
   `value_media_id` int(11) unsigned DEFAULT NULL,
   `is_published` tinyint(1) unsigned NOT NULL DEFAULT '0',
   `created` datetime NOT NULL,
