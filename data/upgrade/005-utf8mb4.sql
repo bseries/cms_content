0c4f730813252bdcd8a013f5e6e4584f97a4b459
Marius Wilms <mariuswilms@mailbox.org>
2018-06-17 21:07:24 +0200

    Include utf8mb4 migration

---
+++ data/upgrade/005-utf8mb4.sql
@@ -0,0 +1,4 @@
+ALTER TABLE content_blocks CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE content_blocks DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE pages CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE pages DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
